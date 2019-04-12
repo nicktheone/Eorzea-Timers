2949501dcb1827de0f549208aefc1509076a359d
Nicholas Martini <nicho.martini@gmail.com>
2019-04-12 10:03:20 +0200

    Added Nodes class.

---
+++ Eorzea Timers/Nodes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eorzea_Timers
+{
+    class Nodes
+    {
+        #region JSON
+        public class Reduce
+        {
+            public string item { get; set; }
+            public int icon { get; set; }
+        }
+
+        public class Item
+        {
+            public string item { get; set; }
+            public int icon { get; set; }
+            public int id { get; set; }
+            public string slot { get; set; }
+            public string scrip { get; set; }
+            public Reduce reduce { get; set; }
+        }
+
+        public class Node
+        {
+            public string type { get; set; }
+            public string func { get; set; }
+            public List<Item> items { get; set; }
+            public int stars { get; set; }
+            public List<int> time { get; set; }
+            public string title { get; set; }
+            public string zone { get; set; }
+            public List<int> coords { get; set; }
+            public string name { get; set; }
+            public int uptime { get; set; }
+            public int lvl { get; set; }
+            public int id { get; set; }
+            public double patch { get; set; }
+            public string condition { get; set; }
+            public string bonus { get; set; }
+        }
+        #endregion
+    }
+}
