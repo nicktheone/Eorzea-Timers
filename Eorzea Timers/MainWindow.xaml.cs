ab282c59bd53d32b0d693d63b4211dc68edd3565
Nicholas Martini <nicho.martini@gmail.com>
2019-04-12 10:05:41 +0200

    Added ToEorzeaTime() extension.

--- Eorzea Timers/MainWindow.xaml.cs
+++ Eorzea Timers/MainWindow.xaml.cs
@@ -24,5 +24,32 @@ namespace Eorzea_Timers
         {
             InitializeComponent();
         }
+
+        #region Methods
+
+        #endregion
+
+        #region Events
+
+        #endregion
+    }
+
+    public static class Extension
+    {
+        public static DateTime ToEorzeaTime(this DateTime date)
+        {
+            //https://www.reddit.com/r/ffxiv/comments/2pbl8p/eorzea_time_formula/cmvijkz?utm_source=share&utm_medium=web2x
+            //https://olitee.com/2015/01/c-convert-current-time-ffxivs-eorzea-time/
+
+            const double EorzeaMultiplier = 3600d / 175d;
+
+            // Calculate how many ticks have elapsed since 1/1/1970
+            long epochTicks = date.ToUniversalTime().Ticks - (new DateTime(1970, 1, 1).Ticks);
+
+            // Multiply those ticks by the Eorzea multipler (approx 20.5x)
+            long eorzeaTicks = (long)Math.Round(epochTicks * EorzeaMultiplier);
+
+            return new DateTime(eorzeaTicks);
+        }
     }
 }
